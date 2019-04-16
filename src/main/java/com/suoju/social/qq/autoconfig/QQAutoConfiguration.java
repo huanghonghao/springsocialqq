c0d9c89790212e87f306bb719fd93a587535414d
huanghonghao <1250659520@qq.com>
2019-04-16 08:13:21 +0800

    test

--- src/main/java/com/suoju/social/qq/autoconfig/QQAutoConfiguration.java
+++ src/main/java/com/suoju/social/qq/autoconfig/QQAutoConfiguration.java
@@ -3,8 +3,10 @@ package com.suoju.social.qq.autoconfig;
 import com.suoju.social.qq.api.QQ;
 import com.suoju.social.qq.content.QQConnectionFactory;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -14,34 +16,90 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.core.env.Environment;
+import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.social.UserIdSource;
 import org.springframework.social.config.annotation.ConnectionFactoryConfigurer;
 import org.springframework.social.config.annotation.EnableSocial;
+import org.springframework.social.config.annotation.SocialConfiguration;
 import org.springframework.social.config.annotation.SocialConfigurerAdapter;
 import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.connect.UsersConnectionRepository;
+import org.springframework.social.connect.jdbc.JdbcUsersConnectionRepository;
+import org.springframework.social.security.AuthenticationNameUserIdSource;
+import org.springframework.social.security.SpringSocialConfigurer;
+
+import javax.sql.DataSource;
 
 @Slf4j
 @EnableSocial
 @Configuration
 @ConditionalOnClass(value = {SocialConfigurerAdapter.class, QQConnectionFactory.class})
 @ConditionalOnProperty(prefix = "spring.social.qq.", value = "app-id")
+@AutoConfigureBefore(SocialConfiguration.class)
 @AutoConfigureAfter(WebMvcAutoConfiguration.class)
 public class QQAutoConfiguration extends SocialConfigurerAdapter {
 
     @Value("${spring.social.qq.app-id}")
     private String qqAppId;
 
-    @Value("${ssb.security.social.qq.app-secret}")
+    @Value("${spring.social.qq.app-secret}")
     private String qqAppSecret;
 
-    @Value("${ssb.security.social.qq.provider-id}")
+    @Value("${spring.social.qq.provider-id}")
     private String qqProviderId;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    public UserIdSource getUserIdSource() {
+        return new AuthenticationNameUserIdSource();
+    }
 
     @Override
     public void addConnectionFactories(ConnectionFactoryConfigurer configurer, Environment environment) {
         configurer.addConnectionFactory(new QQConnectionFactory("qq", qqAppId, qqAppSecret));
     }
 
+    @Override
+    public UsersConnectionRepository getUsersConnectionRepository(ConnectionFactoryLocator connectionFactoryLocator) {
+        JdbcUsersConnectionRepository repository = new JdbcUsersConnectionRepository(dataSource,
+                connectionFactoryLocator, Encryptors.noOpText());
+//        if (myConnectionSignUp != null) {
+//            repository.setConnectionSignUp(myConnectionSignUp);
+//        }
+        return repository;
+    }
+
+    /**
+     * 社交登录配类
+     *
+     * @return
+     */
+    @Bean
+    public SpringSocialConfigurer socialSecurityConfig() {
+        return new SpringSocialConfigurer();
+    }
+
+    /*    @Bean
+    public SpringSocialConfigurer merryyouSocialSecurityConfig() {
+        String filterProcessesUrl = SecurityConstants.DEFAULT_SOCIAL_PROCESS_URL;
+        MerryyouSpringSocialConfigurer configurer = new MerryyouSpringSocialConfigurer(filterProcessesUrl);
+        return configurer;
+    }*/
+
+    /**
+     * 处理注册流程的工具类
+     *
+     * @param factoryLocator
+     * @return
+     */
+/*    @Bean
+    public ProviderSignInUtils providerSignInUtils(ConnectionFactoryLocator factoryLocator) {
+        return new ProviderSignInUtils(factoryLocator, getUsersConnectionRepository(factoryLocator));
+    }*/
     @Bean
     @ConditionalOnMissingBean(QQ.class)
     @Scope(value = "request", proxyMode = ScopedProxyMode.INTERFACES)
